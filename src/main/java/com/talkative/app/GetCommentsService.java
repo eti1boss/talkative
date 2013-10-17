987bf0171fc8f8f23ccbdb661f5041aee11494d4
Etienne Bossuet <e.bossuet@gmail.com>
2013-10-17 16:13:20 +0200

    Test sur @GET @POST avec form html : OK

--- src/main/java/com/talkative/app/GetCommentsService.java
+++ src/main/java/com/talkative/app/GetCommentsService.java
@@ -1,5 +1,6 @@
 package com.talkative.app;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -7,14 +8,17 @@ import javax.ws.rs.Path;
 @Path("/getComments")
 public class GetCommentsService {
 	
-	@GET
-    public String getComments() {
-        return "Aucun commentaire pour cet article";
-    }
+//	@GET
+//    public String getComments() {
+//        return "Aucun commentaire pour cet article";
+//    }
 	
-	@POST
-	public String getParam(String name){
-		return name;
+	@GET
+	public String getParam(
+			@FormParam("test") String test,
+			@FormParam("test2") String test2
+			){
+		return test+":"+test2;
 	}
 
 }
