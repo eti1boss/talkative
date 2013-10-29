0dd737bdd1e23b6626ae9ec621db3b2bcc722798
qvoiriot <qvoiriot@gmail.com>
2013-10-29 14:14:31 +0100

    add a returnForm in js

---
+++ talkative_html/app/assets/js/main.js
@@ -0,0 +1,6 @@
+function ReturnForm(talkative) {
+var name = document.getElementById('InputName').value;
+var Email = document.getElementById('InputEmail').value;
+var Comment = document.getElementById('commentZone').value;
+alert("Thanks for the message : " + name);
+}
